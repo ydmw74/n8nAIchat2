9947c66cd6708873a72211ca212d9160ed3233a5
„ydmw74“ <markus.werle@gmail.com>
2025-02-16 11:30:59 +0100

    Changes to be committed: 	modified:   src/config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  webhookUrl: '<YOUR WEBHOOK URL HERE>'
+  webhookUrl: 'http://nai.amhomenet.de:5678/webhook/cdb5c076-d458-4b9d-8398-f43bd25059b1/chat'
 };
 
 export default config;
