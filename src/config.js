b1a90386093c80608f4e86c6a224431f5c8f8c91
ydmw74 <119453475+ydmw74@users.noreply.github.com>
2025-02-16 15:44:11 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  webhookUrl: 'http://nai.amhomenet.de:5678/webhook/e7753995-345e-4ee9-9752-b10dfcdf80ab',
+  webhookUrl: 'http://www.example.com:5678/webhook/e7753995-345e-4ee9-9752-b10dfcdf80ab',
   inputField: 'chatInput'   // Field name for input in request
 };
 
